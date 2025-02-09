02a6934a8a46c5608f393c4cc28b0dc236ca149d
Kyumin Kim <lemon6565@naver.com>
2025-02-10 08:42:50 +0900

    Docs: 기본 설정 파일

---
+++ src/main/java/com/eodi/yak/eodi_yak/EodiYakApplication.java
@@ -0,0 +1,13 @@
+package com.eodi.yak.eodi_yak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodiYakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodiYakApplication.class, args);
+	}
+
+}
