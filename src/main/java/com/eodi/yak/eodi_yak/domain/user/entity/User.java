e2c1f9b65ddce577982e342abc5e50b5f84cd126
Kyumin Kim <lemon6565@naver.com>
2025-02-09 22:32:30 +0900

    Feat: User entity 코드 작성

---
+++ src/main/java/com/eodi/yak/eodi_yak/domain/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.eodi.yak.eodi_yak.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Builder
+    public User(String userEmail, String password, String phoneNumber){
+        this.userEmail = userEmail;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+}
