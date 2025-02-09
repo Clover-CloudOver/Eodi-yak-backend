ec9c6fdc0eafe415a13c9ca7807072ca5cecd0a3
Kyumin Kim <lemon6565@naver.com>
2025-02-10 08:43:57 +0900

    Chore: Test 파일 추가

---
+++ src/test/java/com/eodi/yak/eodi_yak/EodiYakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eodi.yak.eodi_yak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodiYakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
