0e675b9f4631a326ad7e1c73dda940f5d7a93600
Kyumin Kim <lemon6565@naver.com>
2025-02-18 15:20:09 +0900

    Feat: 약국 데이터에 위도 경도 추가

--- src/main/java/com/eodi/yak/eodi_yak/domain/medicine/response/MedicineResponse.java
+++ src/main/java/com/eodi/yak/eodi_yak/domain/medicine/response/MedicineResponse.java
@@ -3,6 +3,7 @@ package com.eodi.yak.eodi_yak.domain.medicine.response;
 import com.eodi.yak.eodi_yak.domain.medicine.entity.Medicine;
 import lombok.Builder;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,6 +11,8 @@ public record MedicineResponse(
         String meName,
         String paCode,
         Integer meCount,
+        BigDecimal paLatitude, // 약국 위도
+        BigDecimal paLongitude, // 약국 경도
         LocalDateTime updatedAt
 ) {
     public static MedicineResponse from(Medicine medicine) {
@@ -17,6 +20,8 @@ public record MedicineResponse(
                 .meName(medicine.getId().getMeName())
                 .paCode(medicine.getId().getPaCode())
                 .meCount(medicine.getStock())
+                .paLatitude(medicine.getPharmacy().getLatitude())
+                .paLongitude(medicine.getPharmacy().getLongitude())
                 .updatedAt(medicine.getUpdatedAt())
                 .build();
     }
