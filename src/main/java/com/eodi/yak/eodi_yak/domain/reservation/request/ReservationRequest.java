7cba414f826dbe3caa7fb4d35ccadecfce2b5eef
Kyumin Kim <lemon6565@naver.com>
2025-02-18 16:49:36 +0900

    Chore: swagger 기본값 변경

--- src/main/java/com/eodi/yak/eodi_yak/domain/reservation/request/ReservationRequest.java
+++ src/main/java/com/eodi/yak/eodi_yak/domain/reservation/request/ReservationRequest.java
@@ -9,7 +9,7 @@ public record ReservationRequest(
         @Schema(description = "사용자 고유 id(자동부여)", defaultValue = "1")
         String memberId,
 
-        @Schema(description = "약국 코드", defaultValue = "JDQ4MTg4MSM1MSMkMSMkMCMkMDMkNDgxMzUxIzIxIyQyIyQ5IyQwMCQ0NjE0ODEjODEjJDEjJDIjJDgz")
+        @Schema(description = "약국 코드", defaultValue = "002")//"JDQ4MTg4MSM1MSMkMSMkMCMkMDMkNDgxMzUxIzIxIyQyIyQ5IyQwMCQ0NjE0ODEjODEjJDEjJDIjJDgz")
         String pharmacyCode,
 
         @Schema(description = "약 이름", defaultValue = "타이레놀")
