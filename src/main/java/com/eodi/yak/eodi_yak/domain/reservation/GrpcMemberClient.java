f3c2e75096b679bd3ecb9cc6e94ce9df6ea7cdfe
Kyumin Kim <lemon6565@naver.com>
2025-03-06 16:44:14 +0900

    Feat: gRPC member client 추가

---
+++ src/main/java/com/eodi/yak/eodi_yak/domain/reservation/GrpcMemberClient.java
@@ -0,0 +1,25 @@
+package com.eodi.yak.eodi_yak.domain.reservation;
+
+import io.grpc.ManagedChannel;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+//import com.eodi.yak.eodi_yak.domain.reservation.request.ReservationRequest;
+
+import member.Member;
+import member.MemberServiceGrpc;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GrpcMemberClient {
+    private final MemberServiceGrpc.MemberServiceBlockingStub stub;
+
+    public GrpcMemberClient() {
+        ManagedChannel channel = NettyChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+        stub = MemberServiceGrpc.newBlockingStub(channel);
+    }
+
+    public Member.MemberResponse getMemberId(Member.MemberRequest request) {
+        return stub.getMemberById(request);
+    }
+}
